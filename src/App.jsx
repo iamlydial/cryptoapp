e27b8e53be2c0f204cbb0747d146a8dcb31b4760
iamlydial <lydia.lavecchia@gmail.com>
2023-09-12 15:55:51 +0200

    started navbar component with centralized index.js export default for all components

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,19 @@
-import "./App.css";
+import React from "react";
+// import { Switch, Route, Link } from "react-router-dom";
+// import { Layout, Typography, Space } from "antd";
 
+import { Navbar } from "./components";
+import "./app.css";
 const App = () => {
   return (
     <>
-      <h1>App</h1>
+      <div className="app">
+        <div className="navbar">
+          <Navbar />
+        </div>
+        <div className="main"></div>
+        <div className="footer"></div>
+      </div>
     </>
   );
 };
