4ab8330b069c57264936ed33ab4275a3b5b9fddd
iamlydial <lydia.lavecchia@gmail.com>
2023-09-13 11:45:18 +0200

    added compoenents

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,11 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import { BrowserRouter as Router } from "react-router-dom";
 
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <Router>
-    <App />
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
   </Router>
 );
