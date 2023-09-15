4127efe1cbe093b34ca600ce513e8dfd3127e0ee
iamlydial <lydia.lavecchia@gmail.com>
2023-09-15 18:50:22 +0200

    fixed env local file

--- src/services/cryptoApi.js
+++ src/services/cryptoApi.js
@@ -1,8 +1,8 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 const cryptoApiHeaders = {
-  "X-RapidAPI-Host": process.env.REACT_APP_CRYPTO_RAPIDAPI_HOST,
-  "X-RapidAPI-Key": process.env.REACT_APP_RAPIDAPI_KEY,
+  "X-RapidAPI-Host": import.meta.env.VITE_REACT_APP_CRYPTO_RAPIDAPI_HOST,
+  "X-RapidAPI-Key": import.meta.env.VITE_REACT_APP_RAPIDAPI_KEY,
 };
 
 const baseUrl = "https://coinranking1.p.rapidapi.com/coins";
