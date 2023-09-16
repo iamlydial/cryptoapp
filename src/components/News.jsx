445f7762ca73890903b7e2361a06489923b7b495
iamlydial <lydia.lavecchia@gmail.com>
2023-09-16 14:25:13 +0200

    added store reducer for the crypto api news plus fetched the data inthe news components

--- src/components/News.jsx
+++ src/components/News.jsx
@@ -1,6 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
+import { Select, Typography, Row, Col, Avatar, Card } from "antd";
+import moment from "moment";
 
-const News = () => {
+import { useGetCryptoNewsQuery } from "../services/cryptoNewsApi";
+
+const { Text, Title } = Typography;
+const { Option } = Select;
+
+const News = ({ simplified }) => {
+  const [newsCategory, setNewsCategory] = useState("Cryptocurrency");
+  const count = simplified ? 10 : 100;
+
+  const {
+    data: cryptoNews,
+    isFetching,
+    isError,
+  } = useGetCryptoNewsQuery({
+    newsCategory,
+    count,
+  });
+
+  if (isFetching) {
+    return <div>Loading...</div>;
+  }
+
+  if (isError) {
+    return <div>Error fetching data</div>;
+  }
+
+  console.log("API Response", cryptoNews);
+
   return <div>News</div>;
 };
 
