1644600d9ee6f0980369d40fd5ad6ffc80e90c4b
iamlydial <lydia.lavecchia@gmail.com>
2023-09-17 16:38:46 +0200

    added map function to showcase news in the news age

--- src/components/News.jsx
+++ src/components/News.jsx
@@ -9,7 +9,7 @@ const { Option } = Select;
 
 const News = ({ simplified }) => {
   const [newsCategory, setNewsCategory] = useState("Cryptocurrency");
-  const count = simplified ? 10 : 100;
+  const count = simplified ? 6 : 12;
 
   const {
     data: cryptoNews,
@@ -30,7 +30,23 @@ const News = ({ simplified }) => {
 
   console.log("API Response", cryptoNews);
 
-  return <div>News</div>;
+  return (
+    <Row gutter={[24, 24]}>
+      {cryptoNews.value.map((news, i) => (
+        <Col xs={24} sm={12} lg={6} key={i}>
+          <Card hoverable className="news-card">
+            <a href={news.url} target="_blank" rel="nonreferrer">
+              <div className="news-image-container">
+                <Title className="news-title" level={4}>
+                  {news.name}
+                </Title>
+              </div>
+            </a>
+          </Card>
+        </Col>
+      ))}
+    </Row>
+  );
 };
 
 export default News;
