9d9da612c7b8f3dd7ffbe31e6e90a9c5deada361
iamlydial <lydia.lavecchia@gmail.com>
2023-09-17 17:11:21 +0200

    added drop down to crypto news, now user is able to filter news by the crypto of interest

--- src/components/News.jsx
+++ src/components/News.jsx
@@ -3,6 +3,7 @@ import { Select, Typography, Row, Col, Avatar, Card } from "antd";
 import moment from "moment";
 
 import { useGetCryptoNewsQuery } from "../services/cryptoNewsApi";
+import { useGetCryptosQuery } from "../services/cryptoApi";
 
 const { Text, Title } = Typography;
 const { Option } = Select;
@@ -11,8 +12,9 @@ const demoImage =
   "https://www.bing.com/th?id=OVFT.mpzuVZnv8dwIMRfQGPbOPC&pid=News";
 
 const News = ({ simplified }) => {
-  const [newsCategory, setNewsCategory] = useState("Cryptocurrency");
+  const [newsCategory, setNewsCategory] = useState("Cryptocrurrency");
   const count = simplified ? 6 : 12;
+  const { data } = useGetCryptosQuery(100);
 
   const {
     data: cryptoNews,
@@ -35,6 +37,25 @@ const News = ({ simplified }) => {
 
   return (
     <Row gutter={[24, 24]}>
+      {!simplified && (
+        <Col span={24}>
+          <Select
+            showSearch
+            className="select-news"
+            placeholder="Select a Crypto"
+            optionFilterProp="children"
+            onChange={(value) => console.log(value)}
+            filteringOption={(input, option) =>
+              option.children.toLowerCase().indexOf(input.toLowerCase()) >= 0
+            }
+          >
+            <Option value="Cryptocurrency">Cryptocurrency</Option>
+            {data?.data?.coins.map((coin) => (
+              <Option value={coin.name}>{coin.name}</Option>
+            ))}
+          </Select>
+        </Col>
+      )}
       {cryptoNews.value.map((news, i) => (
         <Col xs={24} sm={12} lg={6} key={i}>
           <Card hoverable className="news-card">
